557994c0dec6c34a90d14114637e4b896c0ff4d3
Kevin Thor Hansen <ktha20@student.aau.dk>
2021-04-14 13:50:36 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Ktha20:990525Kh@cluster0.xrq94.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
